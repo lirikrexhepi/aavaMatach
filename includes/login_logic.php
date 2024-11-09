0cbf220812f4947ef8bdc78e1a1e9d91cd839f84
lirik <lirikrexhepiii@gmail.com>
2024-11-09 12:31:19 +0100

    Files Update and Connect
    Connected the files in between for the user and did skibidi boom

--- includes/login_logic.php
+++ includes/login_logic.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
             echo json_encode([
                 'success' => true,
-                'redirect' => 'dashboard.php'
+                'redirect' => 'swipe.html'
             ]);
             exit;
         }
